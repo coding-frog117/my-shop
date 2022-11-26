c8c943646430a1ef9f8703e06efac98e87087b14
coding-frog117 <syeshe2@gmail.com>
2022-11-26 23:38:55 +0900

    useEffect사용해서 요소 없어지게 하기

--- src/App.js
+++ src/App.js
@@ -3,20 +3,34 @@ import './App.css';
 import data from './data';
 import {useState} from 'react';
 import ProductList from './Component/ProductList';
-import {Routes, Route, Link} from 'react-router-dom'
-import detail from './Component/datail';
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import Detail from './Pages/Detail';
 
 function App() {
   let [clothes]=useState(data); 
+  let navigate=useNavigate();
+
   return (
     <div className="shop">
       <nav className="shop-nav">
         <ul className="nav-list">
           <li className="header">
-            <h1>예은쇼핑몰</h1></li>
+            <h1 onClick={()=>{
+              navigate('/');
+            }}>예은쇼핑몰</h1></li>
           <li>outer</li>
           <li>top</li>
           <li>bottom</li>
+          <li onClick={()=>{
+            navigate('/detail');
+          }}>Detail</li>
+
+          <li onClick={()=>{
+            navigate(-1);
+          }}>뒤로가기</li>
+          <li onClick={()=>{
+            navigate(1);
+          }}>앞으로가기</li>
         </ul>
       </nav>
 
@@ -43,14 +57,31 @@ function App() {
           
       </>}></Route>
 
-        <Route path="/detail" element={detail()}></Route>
-      </Routes>
+        <Route path="/detail" element={<Detail clothes={clothes}  ></Detail>}></Route>
+        <Route path="/detail/:id" element={<Detail clothes={clothes}></Detail>}></Route>
+        <Route path="*" element={
+          <div>없는 페이지입니다. 404 error</div>
+        }></Route>
 
-      
+        <Route path="/event" element={<Event></Event>}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
+          <Route path="two" element={<div>생일 기념 쿠폰 받기</div>}></Route>
+        </Route>
+      </Routes> 
+    
     </div>
 
   );
 }
+
+function Event(){
+  return(
+  <div>오늘의 이벤트
+    <Outlet></Outlet>
+  </div>
+  )
+}
+
 
 
 
---
+++ src/Pages/Detail.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
+import styled from 'styled-components';
+
+let YellowBtn= styled.button`
+    background: ${props=>props.bg};
+    color: ${props=>props.bg==="blue"? 'white' : 'black'};
+    padding:10px;
+`
+
+function Detail(props){
+    useEffect(()=>{
+        setTimeout(()=>{setAlert(false)},2000)
+    })
+
+    let [count,setCount]=useState(0);
+    let [alert,setAlert]=useState(true);  
+    
+    let {id}=useParams();
+    console.log(id);
+    const found=props.clothes.find(index=>
+        index.id==id
+    )
+
+    return(
+    <div className="container">
+        {alert==true
+        ?<div className="alert alert-warning">2초 이내 구매시 할인</div>
+        : null
+        }
+        <YellowBtn bg="blue" onClick={setCount(count+1)}>{count}</YellowBtn>
+        <YellowBtn bg="yellow"></YellowBtn>
+        <YellowBtn bg="pink"></YellowBtn>
+
+    <Link to="/">홈</Link>
+    <div className="row">
+        <div className="col-md-6">
+        <img src={process.env.PUBLIC_URL+`/img/clothes_${id}.jpg`} width="100%" />
+        </div>
+        <div className="col-md-6">
+        <h4 className="pt-5">
+            {found.title}
+        </h4>
+        <p>{found.content}</p>
+        <p>{found.price}</p>
+        <button className="btn btn-danger">주문하기</button> 
+        </div>
+    </div>
+    </div> 
+    )
+}
+
+export default Detail;
